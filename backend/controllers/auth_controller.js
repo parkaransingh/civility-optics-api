b0a60f77aba3261a1369916399d2e111c6bce7ac
mgebremariam7 <mgebremariam7@gatech.edu>
2022-10-19 13:30:20 -0400

    added new function to allow business accounts to be authorized

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -1,4 +1,5 @@
 import jwt from 'jsonwebtoken'
+import Business from '../models/business_model.js'
 
 import User from '../models/users_model.js'
 
@@ -13,6 +14,25 @@ const auth = async(req, res, next) => {
             throw new Error()
         }
         req.user = user
+        req.token = token
+        next()
+    } catch (error) {
+        res.status(401).send({ error: 'Not authorized to access this resource' })
+    }
+
+}
+
+const authBusiness = async(req, res, next) => {
+    console.log("Request Body")
+    console.log(req.body)
+    const token = req.header('Authorization').replace('Bearer ', '')
+    const data = jwt.verify(token, process.env.JWT_KEY)
+    try {
+        const business = await Business.findOne({ _id: data._id, 'tokens.token': token })
+        if (!business) {
+            throw new Error()
+        }
+        req.business = business
         req.token = token
         next()
     } catch (error) {
