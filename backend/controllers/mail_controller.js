42eec52970453f9785b1c09f6451b155d08423ac
Xander Goebel <xandergoebel1@gmail.com>
2022-10-21 12:51:55 -0400

    refactoring routes and mail functions for clarity

--- backend/controllers/mail_controller.js
+++ backend/controllers/mail_controller.js
@@ -2,44 +2,53 @@ import nodemailer from 'nodemailer'
 import asyncHandler from 'express-async-handler'
 import google from 'googleapis'
 
-export const sendMail = asyncHandler(async (req, res) => {
+export const sendCode = asyncHandler(async (req, res) => {
   try {
-    const verifyCode = Math.random().toString(36).substring(2, 7)
-    req.user.verificationCode = verifyCode
-    await req.user.save()
-    console.log('User verification code saved')
+    if (!req.user.isVerified) {
+      // Generate a random alphanumeric code
+      const verifyCode = Math.random().toString(36).substring(2, 7)
+      // Save it to relation for verification purposes
+      req.user.verificationCode = verifyCode
+      await req.user.save()
+      console.log('User verification code saved')
 
-    const oAuth2Client = new google.Auth.OAuth2Client(
-      process.env.OAUTH_CLIENT_ID,
-      process.env.OAUTH_CLIENT_SECRET,
-      process.env.OAUTH_REDIRECT_URI
-    )
-    oAuth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN })
-    const accToken = await oAuth2Client.getAccessToken()
-    const transport = nodemailer.createTransport({
-      service: 'gmail',
-      auth: {
-        type: 'OAuth2',
-        user: process.env.EHOST,
-        clientId: process.env.OAUTH_CLIENT_ID,
-        clientSecret: process.env.OAUTH_CLIENT_SECRET,
-        refreshToken: process.env.OAUTH_REFRESH_TOKEN,
-        accessToken: accToken
+      // Set up email authentification client
+      // Note: Google may deprecate, so keep up to date
+      const oAuth2Client = new google.Auth.OAuth2Client(
+        process.env.OAUTH_CLIENT_ID,
+        process.env.OAUTH_CLIENT_SECRET,
+        process.env.OAUTH_REDIRECT_URI
+      )
+      // Use credentials from .env file
+      oAuth2Client.setCredentials({ refresh_token: process.env.OAUTH_REFRESH_TOKEN })
+      const accToken = await oAuth2Client.getAccessToken()
+      const transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: process.env.EHOST,
+          clientId: process.env.OAUTH_CLIENT_ID,
+          clientSecret: process.env.OAUTH_CLIENT_SECRET,
+          refreshToken: process.env.OAUTH_REFRESH_TOKEN,
+          accessToken: accToken
+        }
+      })
+      // Generate message containing code
+      const message = {
+        from: '"Civility Optics" <no-reply@civilityoptics.com>',
+        to: req.user.email,
+        subject: 'Verify Email for "Civility Optics" app',
+        html: '<p>You requested for email verification, kindly use this code "' + verifyCode +
+        '" to verify your email address on the app</p>'
       }
-    })
-    const message = {
-      from: '"Civility Optics" <no-reply@civilityoptics.com>',
-      to: req.user.email,
-      subject: 'Verify Email for "Civility Optics" app',
-      html: '<p>You requested for email verification, kindly use this code "' + verifyCode +
-      '" to verify your email address on the app</p>'
+      // Send using nodemailer client
+      transport.sendMail(message, (error, info) => {
+        if (error) {
+          return console.log(error)
+        }
+        console.log('Message sent: %s', info.messageId)
+      })
     }
-    transport.sendMail(message, (error, info) => {
-      if (error) {
-        return console.log(error)
-      }
-      console.log('Message sent: %s', info.messageId)
-    })
 
     res.send()
   } catch (error) {
@@ -49,13 +58,15 @@ export const sendMail = asyncHandler(async (req, res) => {
 
 export const verifyCode = asyncHandler(async (req, res) => {
   try {
-    const verifyCode = req.body.verifycode
-    if (req.user.verificationCode !== undefined && verifyCode === req.user.verificationCode) {
-      req.user.isVerified = true
-      req.user.verificationCode = undefined
-      await req.user.save()
-    } else {
-      res.status(403).send({ error: 'Verification code does not match' })
+    if (!req.user.isVerified) {
+      const verifyCode = req.body.verifycode
+      if (req.user.verificationCode !== undefined && verifyCode === req.user.verificationCode) {
+        req.user.isVerified = true
+        req.user.verificationCode = undefined
+        await req.user.save()
+      } else {
+        res.status(403).send({ error: 'Verification code does not match' })
+      }
     }
 
     res.send()
--- backend/routes/users_route.js
+++ backend/routes/users_route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import auth from '../controllers/auth_controller.js'
-import { sendMail, verifyCode } from '../controllers/mail_controller.js'
+import { sendCode, verifyCode } from '../controllers/mail_controller.js'
 import { postUser, getUser, loginUser, logoutUser, logoutUserAll } from '../controllers/users_controller.js'
 
 const userRouter = express.Router()
@@ -15,8 +15,8 @@ userRouter.route('/users/me/logout').post(auth, logoutUser)
 
 userRouter.route('/users/me/logoutall').post(auth, logoutUserAll)
 
-userRouter.route('/users/verify/send').post(auth, sendMail)
+userRouter.route('/users/me/sendcode').post(auth, sendCode)
 
-userRouter.route('/users/verify/receive').post(auth, verifyCode)
+userRouter.route('/users/me/verifycode').post(auth, verifyCode)
 
 export default userRouter
